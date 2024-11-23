8bada388d4b9a841f80f93efb6dfa349968451fb
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-23 23:46:02 +0800

    up

--- hosts/alibaba-hk/network.nix
+++ hosts/alibaba-hk/network.nix
@@ -5,12 +5,16 @@
 }:
 {
   systemd.network.networks.eth0 = {
-    DHCP = "yes";
+    address = [
+      "172.19.16.213/18"
+    ];
+    gateway = [
+      "172.19.63.253"
+    ];
     matchConfig.Name = "eth0";
   };
 
   networking.nameservers = [
     "8.8.8.8"
-    "2001:4860:4860::8888"
   ];
 }
