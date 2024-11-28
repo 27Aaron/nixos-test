0ac516672cbc73421bf6991ba7c1c5e56a178252
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-11-28 09:42:52 +0800

    up

--- hosts/alibaba-hk/network.nix
+++ hosts/alibaba-hk/network.nix
@@ -6,15 +6,28 @@
 {
   systemd.network.networks.eth0 = {
     address = [
-      "172.19.16.213/18"
+      "93.123.118.106/24"
+      "2a0f:ca80:1337:9262::1/64"
     ];
     gateway = [
-      "172.19.63.253"
+      "93.123.118.1"
+    ];
+    routes = [
+      {
+        # Special config since gateway isn't in subnet
+        Destination = "::/0";
+        Gateway = "2a0f:ca80:1337::1";
+        GatewayOnLink = true;
+      }
     ];
     matchConfig.Name = "eth0";
+    networkConfig = {
+      IPv6AcceptRA = false;
+    };
   };
 
   networking.nameservers = [
     "8.8.8.8"
+    "2001:4860:4860::8888"
   ];
 }
